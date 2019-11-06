ae8d3ecfb30d33f16f62506a5089ea312a1e918c
rodolfo.lacerda <rodolfo.lacerda@memt.com.br>
2019-11-05 21:34:43 -0300

    teste

--- tests/busca/login.test.js
+++ tests/busca/login.test.js
@@ -4,11 +4,37 @@ module.exports = {
     'ACESSO AO SISTEMA SMARTYENERGY CADASTRO OKk': function (browser) {
         var userInfo = '.user .info span'
         browser
-            .resizeWindow(1920, 1080)
-            .url('http://187.72.82.243:3000')
-            .waitForElementVisible('.MuiIconButton-edgeStart', 3000)
-            .click('.MuiIconButton-colorSecondary')
-           
-            .end();
+        .url('http://187.72.82.243:3000')
+        .waitForElementVisible('.MuiIconButton-edgeStart', 3000)
+        .click('.MuiIconButton-colorSecondary')
+        .useXpath()
+        .waitForElementVisible('//span[contains(.,"Contratos")]', 3000)
+        .click('//span[contains(.,"Contratos")]')
+        .useXpath()
+        .waitForElementVisible('.//div/div/div/a/button/span', 3000)
+        .click('.//div/div/div/a/button/span')
+        .useXpath()
+        .waitForElementVisible('//div[@id="select-use_contract_type.id_use_contract_type"]', 3000)
+        .click('//div[@id="select-use_contract_type.id_use_contract_type"]')
+        .useXpath()
+        .waitForElementVisible('//li[contains(.,"Distribuição")]', 3000)
+        .click('//li[contains(.,"Distribuição")]')
+        .keys(browser.Keys.TAB)
+        .useXpath()
+        .waitForElementVisible('//div[@id="select-company.id_company"]', 5000)
+        .keys(browser.Keys.ENTER)
+        .useXpath()
+        .waitForElementVisible('//li[contains(.,"Itabira")]', 2000)
+        .click('//li[contains(.,"Itabira")]')
+        .keys(browser.Keys.TAB)
+        .useXpath()
+        .waitForElementVisible('//div[@id="select-energy_dealer.id_company"]', 2000)
+        .click('//div[@id="select-energy_dealer.id_company"]')
+        
+    
+     
+       
+       
+        .end();
     }
 }
