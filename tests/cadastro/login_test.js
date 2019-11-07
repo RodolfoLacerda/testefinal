89520394ec8942b50c281de16a4ddb8273c59b2d
rodolfo.lacerda <rodolfo.lacerda@memt.com.br>
2019-11-07 13:58:57 -0300

    teste

--- tests/cadastro/login_test.js
+++ tests/cadastro/login_test.js
@@ -1,23 +1,24 @@
 module.exports = {
-    'login com sucesso': (browser) => {
+    'smartyEnergy': (browser) => {
         let login = browser.page.login()
        
 
         browser.resizeWindow(1920, 1080)
 
         login.navigate()
-            .waitForElementVisible('@menu', 3000)
-            .click('@acessarMenu')
-            .waitForElementVisible('@menuContratos', 3000)
-            .click('@menuContratos')
-            .waitForElementVisible('@adicionarContrato', 3000)
-            .click('@adicionarContrato')
-            .waitForElementVisible('@tipoContrato', 3000)
-            .click('@tipoContrato')
-            .useXpath()
-            .waitForElementVisible('//li[contains(.,"Distribuição")]', 3000)
-            .click('//li[contains(.,"Distribuição")]')
-            .setValue('@pontoConexao', '123')
+        .waitForElementVisible('@menu', 3000)
+        .click('@acessarMenu')
+        .waitForElementVisible('@menuContratos', 3000)
+        .click('@menuContratos')
+        .waitForElementVisible('@adicionarContrato', 3000)
+        .click('@adicionarContrato')
+        .waitForElementVisible('@tipoContrato', 3000)
+        .click('@tipoContrato')
+        .useXpath()
+        .waitForElementVisible('//li[contains(.,"Distribuição")]', 3000)
+        .click('//li[contains(.,"Distribuição")]')
+        .waitForElementVisible('//div[@id="select-company.id_company"]', 3000)
+        .click('//li[contains(.,"Itabira")]')
 
      
             
--- tests/login/login_test.js
+++ tests/login/login_test.js
@@ -6,19 +6,19 @@ module.exports = {
         browser.resizeWindow(1920, 1080)
 
         login.navigate()
-            .waitForElementVisible('@menu', 3000)
-            .click('@acessarMenu')
-            .waitForElementVisible('@menuContratos', 3000)
-            .click('@menuContratos')
-            .waitForElementVisible('@adicionarContrato', 3000)
-            .click('@adicionarContrato')
-            .waitForElementVisible('@tipoContrato', 3000)
-            .click('@tipoContrato')
-            .useXpath()
-            .waitForElementVisible('//li[contains(.,"Distribuição")]', 3000)
-            .click('//li[contains(.,"Distribuição")]')
-            .setValue('@pontoConexao', '123')
-
+        .waitForElementVisible('@menu', 3000)
+        .click('@acessarMenu')
+        .waitForElementVisible('@menuContratos', 3000)
+        .click('@menuContratos')
+        .waitForElementVisible('@adicionarContrato', 3000)
+        .click('@adicionarContrato')
+        .waitForElementVisible('@tipoContrato', 3000)
+        .click('@tipoContrato')
+        .useXpath()
+        .waitForElementVisible('//li[contains(.,"Distribuição")]', 3000)
+        .click('//li[contains(.,"Distribuição")]')
+        .waitForElementVisible('//div[@id="select-company.id_company"]', 3000)
+        .click('//li[contains(.,"Itabira")]')
             
     }
 }
